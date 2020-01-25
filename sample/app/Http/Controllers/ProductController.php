4e1f0153610939db9b73766767923c86b692072a
kenta-work <kenta.work0822@gmail.com>
2020-01-25 18:13:37 +0900

    コントローラーの追加

---
+++ sample/app/Http/Controllers/ProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //一覧画面
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //商品登録画面
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //商品登録機能
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(product $product)
+    {
+        //商品詳細画面
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(product $product)
+    {
+        //商品編集画面
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, product $product)
+    {
+        //商品編集機能
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(product $product)
+    {
+        //商品削除機能
+    }
+}
